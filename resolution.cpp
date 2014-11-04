5dcca2cbbabbf90fac044f577de91dba964b074b
Quentin DE MUYNCK <quentin.de.14@hotmail.fr>
2014-11-04 19:39:28 +0100

    Ajout fichiers fenetre resolution

---
+++ resolution.cpp
@@ -0,0 +1,31 @@
+#include "resolution.h"
+
+Resolution::Resolution() : QDialog()
+{
+   // setModal(true);
+
+    progression = new QLabel("94");
+    slider = new QSlider(Qt::Horizontal, this);
+
+    ok = new QPushButton(tr("Ok"));
+    cancel = new QPushButton(tr("Annuler"));
+
+    slider->setMaximum(100);
+    slider->setMinimum(1);
+    slider->setValue(94);
+
+    QHBoxLayout *compteur = new QHBoxLayout;
+    compteur->addWidget(slider);
+    compteur->addWidget(progression);
+
+    QHBoxLayout *bouton = new QHBoxLayout;
+    bouton->addWidget(ok, 0, Qt::AlignRight);
+    bouton->addWidget(cancel, 0, Qt::AlignRight);
+
+    QVBoxLayout *final = new QVBoxLayout;
+    final->addLayout(compteur);
+    final->addLayout(bouton);
+
+    setLayout(final);
+    show();
+}
---
+++ resolution.h
@@ -0,0 +1,22 @@
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+#include <QDialog>
+#include <QSlider>
+#include <QPushButton>
+#include <QLabel>
+#include <QLayout>
+
+class Resolution : public QDialog
+{
+    Q_OBJECT
+public:
+    Resolution();
+private:
+    QSlider *slider;
+    QPushButton *ok;
+    QPushButton *cancel;
+    QLabel *progression;
+};
+
+#endif // RESOLUTION_H
