ba9e57f3f86687cb6eb1be2e9c97e78e742c01ba
Quentin DE MUYNCK <quentin.de.14@hotmail.fr>
2014-11-13 21:18:51 +0100

    Amélioration indentation Ajout.cpp

--- ajout.cpp
+++ ajout.cpp
@@ -27,16 +27,16 @@ Ajout::Ajout()
     layout2->addLayout(layout);
     layout2->addWidget(record);
 
-    tableau->setColumnCount(2);
-    tableau->setRowCount(100);
     QStringList labels;
     labels << tr("Termes") << tr("Traductions");
+
+    tableau->setColumnCount(2);
+    tableau->setRowCount(100);
     tableau->setHorizontalHeaderLabels(labels);
     tableau->setColumnWidth(0, 150);
     tableau->setColumnWidth(1, 150);
 
     setLayout(layout2);
-    //resize(485, 500);
     setFixedSize(487, 500);
     setWindowTitle(tr("Ajout d'une fiche"));
 
@@ -49,95 +49,90 @@ void Ajout::enregistre()
     if (langue->text() == "" || titre->text() == "")
     {
         QMessageBox::critical(this, tr("Erreur"), tr("Il manque des informations !"));
-
     }
     else
     {
+        QFile fichier(QCoreApplication::applicationDirPath() + "/data/" + titre->text().toLower() + ".html");
 
-    QFile fichier(QCoreApplication::applicationDirPath() + "/data/" + titre->text().toLower() + ".html");
-    if (!QDir(QCoreApplication::applicationDirPath() + "/data").exists())
-    {
-        QDir().mkdir(QCoreApplication::applicationDirPath() + "/data");
-    }
-    if (fichier.exists())
-    {
-        QMessageBox message;
-        message.setWindowTitle(tr("Confirmation"));
-        message.setText(tr("Cette fiche est déjà existante. Voulez-vous la remplacer ?"));
-        message.addButton(QMessageBox::Yes);
-        message.addButton(QMessageBox::No);
-        message.setButtonText(QMessageBox::Yes, tr("Oui"));
-        message.setButtonText(QMessageBox::No, tr("Non"));
-        int reponse = message.exec();
-        if (reponse == QMessageBox::Yes)
-           {
-               QFile::remove(QCoreApplication::applicationDirPath() + "/data/" + titre->text().toLower() + ".html");
-           }
-           else
-           {
+         if (!QDir(QCoreApplication::applicationDirPath() + "/data").exists())
+         {
+             QDir().mkdir(QCoreApplication::applicationDirPath() + "/data");
+         }
 
-           }
-    }
-    if (!tableau->item(0, 0))
-    {
+         if (fichier.exists())
+         {
+             QMessageBox message;
+             message.setWindowTitle(tr("Confirmation"));
+             message.setText(tr("Cette fiche est déjà existante. Voulez-vous la remplacer ?"));
+             message.addButton(QMessageBox::Yes);
+             message.addButton(QMessageBox::No);
+             message.setButtonText(QMessageBox::Yes, tr("Oui"));
+             message.setButtonText(QMessageBox::No, tr("Non"));
+             int reponse = message.exec();
+              if (reponse == QMessageBox::Yes)
+              {
+                  QFile::remove(QCoreApplication::applicationDirPath() + "/data/" + titre->text().toLower() + ".html");
+              }
+              else
+              {
 
-        QMessageBox::critical(this, tr("Erreur"), tr("Le tableau ne doit pas être vide !"));
+              }
+          }
+          if (!tableau->item(0, 0))
+          {
+              QMessageBox::critical(this, tr("Erreur"), tr("Le tableau ne doit pas être vide !"));
+          }
+          else
+          {
+              if (!fichier.open(QIODevice::WriteOnly | QIODevice::Text))
+              {
+                   QMessageBox::critical(this, tr("Erreur"), tr("Impossibilité de créer le fichier pour l'enregistrement !"));
+              }
+              else
+              {
+                   QTextStream texte(&fichier);
+                   texte.setCodec("UTF-8");
+                   texte << "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\" http-equiv=\"Content-Type\" content=\"text/html\"/>\n<link rel=\"stylesheet\" content=\"text/css\" href=\"..\\style.css\"/>\n</head>\n";
+                   texte << "<body>\n<h1>" << titre->text() << "</h1>\n";
 
-    }
-    else
-    {
-    if (!fichier.open(QIODevice::WriteOnly | QIODevice::Text))
-    {
+                   if (soustitre->text() != "")
+                   {
+                        texte << "<h2 id=\"1\">" << soustitre->text() << "</h2>\n";
+                   }
+                   else
+                   {
+                        texte << "<h2 id=\"0\"></h2>\n";
+                   }
 
-    QMessageBox::critical(this, tr("Erreur"), tr("Impossibilité de créer le fichier pour l'enregistrement !"));
-    }
-    else
-    {
-    QTextStream texte(&fichier);
-    texte.setCodec("UTF-8");
-    texte << "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\" http-equiv=\"Content-Type\" content=\"text/html\"/>\n<link rel=\"stylesheet\" content=\"text/css\" href=\"..\\style.css\"/>\n</head>\n";
-    texte << "<body>\n<h1>" << titre->text() << "</h1>\n";
-    if (soustitre->text() != "")
-    {
-        texte << "<h2 id=\"1\">" << soustitre->text() << "</h2>\n";
-    }
-    else
-    {
-        texte << "<h2 id=\"0\"></h2>\n";
-    }
-    texte << "<div>Langue : " << langue->text() << "</div>\n";
-    texte << "<table>\n<thead>\n<tr>\n<th>Termes</th>\n<th>Traductions</th>\n</tr>\n</thead>\n<tbody>\n";
-    int i;
+                   texte << "<div>Langue : " << langue->text() << "</div>\n";
+                   texte << "<table>\n<thead>\n<tr>\n<th>Termes</th>\n<th>Traductions</th>\n</tr>\n</thead>\n<tbody>\n";
+                   int i;
 
-    for (i = 0 ; i != 99 ; i++)
-    {
+                   for (i = 0 ; i != 99 ; i++)
+                   {
+                         QTableWidgetItem *item = tableau->item(i,0);
+                         QTableWidgetItem *item2 = tableau->item(i, 1);
+                         if (item && item2)
+                         {
+                               texte << "<tr>\n<td>" << tableau->item(i, 0)->text() << "</td>\n<td>" << tableau->item(i, 1)->text() << "</td>\n</tr>";
+                         }
+                         else if (!item && item2)
+                         {
+                               texte << "<tr>\n<td></td>\n<td>" << tableau->item(i, 1)->text() << "</td>\n</tr>";
+                         }
+                         else if (item && !item2)
+                         {
+                               texte << "<tr>\n<td>" << tableau->item(i, 0)->text() << "</td>\n<td> </td>\n</tr>\n";
+                         }
+                         else
+                         {
 
-        QTableWidgetItem *item = tableau->item(i,0);
-        QTableWidgetItem *item2 = tableau->item(i, 1);
-        if (item && item2)
-        {
-            texte << "<tr>\n<td>" << tableau->item(i, 0)->text() << "</td>\n<td>" << tableau->item(i, 1)->text() << "</td>\n</tr>";
-
-        }
-        else if (!item && item2)
-        {
-            texte << "<tr>\n<td></td>\n<td>" << tableau->item(i, 1)->text() << "</td>\n</tr>";
-
-        }
-        else if (item && !item2)
-        {
-            texte << "<tr>\n<td>" << tableau->item(i, 0)->text() << "</td>\n<td> </td>\n</tr>\n";
-
-        }
-        else
-        {
-
-        }
-    }
-    texte << "</tbody>\n</table>\n</body>\n</html>";
-    fichier.close();
-    emit fini();
-    }
-    }
+                         }
+                   }
+                   texte << "</tbody>\n</table>\n</body>\n</html>";
+                   fichier.close();
+                   emit fini();
+              }
+         }
     }
 }
