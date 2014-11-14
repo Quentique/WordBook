bf58e04d1cba8edcaf5635aff17080e7c90d4e5f
Quentin DE MUYNCK <quentin.de.14@hotmail.fr>
2014-11-14 18:46:18 +0100

    Amélioration indentation main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <QIcon>
 #include <QPixmap>
 #include <QDebug>
+
 #include "fenetre.h"
 
 int main(int argc, char *argv[])
@@ -11,24 +12,26 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
-    QTranslator translator;
     QSettings settings(QCoreApplication::applicationDirPath() + "/settings.ini", QSettings::IniFormat);
     int defaut = settings.value("Langue/default").toInt();
     defaut++;
-    qDebug() << translator.load(QCoreApplication::applicationDirPath() + "/langue_"  + settings.value("Langue/" + QString::number(defaut)).toString().toLower().left(2));
+
+    QTranslator translator;
+    translator.load(QCoreApplication::applicationDirPath() + "/langue_"  + settings.value("Langue/" + QString::number(defaut)).toString().toLower().left(2));
+
+    QIcon icone;
+    icone.addPixmap(QPixmap(":/images/icone.png").scaled(120,150));
+    app.setWindowIcon(icone);
     app.installTranslator(&translator);
     app.setApplicationName("WordBook");
+
     Fenetre test;
     if (settings.value("MAJ").toBool())
     {
         test.maj("true");
     }
-    QIcon icone;
-    icone.addPixmap(QPixmap(":/images/icone.png").scaled(120,150));
-    app.setWindowIcon(icone);
 
     test.show();
-    //QString locale = QLocale::system().name().section('_', 0, 0);
 
     return app.exec();
 }
