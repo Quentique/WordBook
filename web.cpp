9499b67ed727fec6a9675dd3bc259284ff3a0aba
Quentin DE MUYNCK <quentin.de.14@hotmail.fr>
2014-11-14 18:58:36 +0100

    Amélioration indentation Web.cpp

--- web.cpp
+++ web.cpp
@@ -4,19 +4,21 @@
 
 Web::Web(QString nomfichier)
 {
-  principale = new QWidget;
-  vue = new QWebView(principale);
   QUrl chemin;
   chemin.setUrl("file:///" + QCoreApplication::applicationDirPath() + "/data/" + nomfichier.toLower() + ".html");
+
+  principale = new QWidget;
+  vue = new QWebView(principale);
   vue->setUrl(chemin);
 
-  principale->show();
-  QSettings *settings = new QSettings(QCoreApplication::applicationDirPath() + "/settings.ini", QSettings::IniFormat);;
+  QSettings *settings = new QSettings(QCoreApplication::applicationDirPath() + "/settings.ini", QSettings::IniFormat);
+
   principale->setMaximumSize(settings->value("Taille/x", 500).toInt(), settings->value("Taille/y", 600).toInt());
   principale->setMinimumSize(settings->value("Taille/x", 500).toInt(), settings->value("Taille/y", 600).toInt());
   vue->setZoomFactor(2);
   vue->setMaximumSize(settings->value("Taille/x", 500).toInt(), settings->value("Taille/y", 600).toInt());
   vue->setMinimumSize(settings->value("Taille/x", 500).toInt(), settings->value("Taille/y", 600).toInt());
   principale->setWindowTitle(QObject::tr("Affichage"));
+  principale->show();
   delete settings;
 }
